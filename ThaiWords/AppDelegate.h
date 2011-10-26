d159fab629d11c2b86559f8f6f7e1c16c11dcd37
xinming <zhaoxmmusic@msn.com>
2011-10-26 22:41:45 +0700

    Initial Commit

---
+++ ThaiWords/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ThaiWords
+//
+//  Created by Xinming Zhao on 10/26/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
